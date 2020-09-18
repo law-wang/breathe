f715959b2968438f7db7adfcb60af584aee22a0d
law-wang <wangcw2001@gmail.com>
2020-09-17 17:46:34 -0700

    fixed clock

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-window.addEventListener('load', function() {
+window.onload = function showTime(){
     var x = new Date();
     var hours = (x.getHours() < 10? '0' : '') + x.getHours();
     var minutes = (x.getMinutes() < 10? '0' : '') + x.getMinutes();
     var seconds = (x.getSeconds() < 10? '0' : '') + x.getSeconds();
     var time = hours + ":" +  minutes + ":" +  seconds;
     document.getElementById('ct').innerHTML = time;
-    display_c();
-})
-
-function display_c(){
-    var refresh = 1000; // Refresh rate in milli seconds
-    mytime = setTimeout('display_ct()', refresh);
+    setTimeout(showTime, 1000);
 }
